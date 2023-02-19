79f7492560226e5e7981112d5d74d2e93092f750
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 03:42:35 -0300

    Add files via upload

---
+++ 36ex soma Par.py
@@ -0,0 +1,14 @@
+
+
+#36. Elabore um programa que calcule e exiba a soma de todos os números pares entre 0 e 10
+
+
+print(' Números Pares Entre 0 a 10\n')
+n = 0
+for  n  in range(-1, 11):
+ if n % 2 == 0:
+        print(n, end =' + ')
+
+t = (0+2+4+6+8+10)
+print(f'= {t}\n')
+
